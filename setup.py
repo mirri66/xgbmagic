2ecb571b013a63f63647fced741db65d67545720
grace <tsmgrace@gmail.com>
2016-06-21 04:53:04 +0000

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='xgbmagic',
-      version='0.0.2.5',
+      version='0.0.2.6',
       description='Data preprocessing and analysis using XGBoost',
       url='http://github.com/mirri66/xgbmagic',
       author='Grace Tang',
